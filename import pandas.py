2d2b2e5a2b6031ed9ca873fcda22439fa3005ec6
Prajwal <pprajwal312@gmail.com>
2023-06-25 11:44:37 +0530

    Pyspark code

--- import pandas.py
+++ import pandas.py
@@ -6,16 +6,16 @@ class Stockmarket:
         import pyotp
         
 
-        self.__api_key=api_key
+        self.api_key=api_key
         self.client_ID=client_ID
         self.password=password
         self.totp=totp
 
-        obj =SmartConnect(api_key=self.__api_key)
+        obj =SmartConnect(api_key=self.api_key)
         data=obj.generateSession(self.client_ID,self.password,pyotp.TOTP(self.totp).now())
         refreshToken= data['data']['refreshToken']
         feedToken=obj.getfeedToken()
-        print(feedToken)
+        # print(feedToken)
         # self.getdata()
 
         try:
@@ -23,8 +23,8 @@ class Stockmarket:
             "exchange": "NSE",
             "symboltoken": "3045",
             "interval": "ONE_DAY",
-            "fromdate": "2023-02-08 00:00", 
-            "todate": "2023-04-21 00:00"
+            "fromdate": "2019-02-08 00:00", 
+            "todate": "2023-04-28 00:00"
             }
             srcdata = obj.getCandleData(historicParam)
             columns = ['TimeStamp','Open','High','Low','Close','Volume']
@@ -38,24 +38,22 @@ class Stockmarket:
         self.Stock='SBI'
         self.df['Stock'] = self.Stock
         self.df=self.df[['Stock','Open','High','Low','Close','Volume','TimeStamp']]
-        # print(self.df)
-        for index,row in self.df:
-            print(index)
+        print(self.df)
+        self.sqlsetup()
 
     def sqlsetup(self):
+        from sqlalchemy import create_engine
         import pyodbc
-        cnxn = pyodbc.connect("DRIVER={ODBC Driver 17 for SQL Server};"
-                      "Server=DESKTOP-0A2HT13;"
-                      "Database=Databricks;"
-                      "UID=prajwal;"
-                      "PWD=Prajwal082;"
-                      "Trusted_Connection=yes;")
-        cursor = cnxn.cursor()
-        # for index,row in self.df:
-        #     print(index,row)
+        engine = create_engine('mssql+pyodbc://prajwal:Prajwal082@DESKTOP-0A2HT13/Databricks?driver=ODBC Driver 17 for SQL Server')
+        try:
+            conn=engine.connect()
+            print("Connection Sucessfull...")
+            # self.df.to_sql("Stock",con=conn,if_exists='replace')
+            print("Data inserted...")
+        except Exception as e:
+            print("Connection failed....{e}".format(e))
 
 
-
-stock=Stockmarket('tEUW4a6X','P381661','4812','QZYS2YLQQLUBXTXVS3S5WCBQBA')
+stock=Stockmarket('hCcnLasN','P381661','4812','QZYS2YLQQLUBXTXVS3S5WCBQBA')
 
 
